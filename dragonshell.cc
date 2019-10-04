28d85af54cd73c4bc98dc417202855bc2dfabdba
Fahad Ahammed <ahaammed@cs.ualberta.ca>
2019-10-04 16:28:03 -0600

    finished background process

--- dragonshell.cc
+++ dragonshell.cc
@@ -6,6 +6,7 @@
 #define PATH_MAX  4096
 #include <sys/wait.h>
 #include <fcntl.h>
+pid_t childPid;
 
 /**
  * @brief Tokenize a string 
@@ -33,26 +34,13 @@ vector<string> tokenize(const string &str, const char *delim) {
 
 
 void pwd() {
-  pid_t pid = fork();
-  if (pid == 0){
-    //cout << "went into pid=0" << endl;
-    char *dir;
+    char dir[PATH_MAX];
     getcwd(dir, PATH_MAX);
     if(dir != NULL) {
       cout << dir << endl;
     } 
     else cout << "failed to get directory" << endl;
     return;
-  }
-  if (pid < 0){
-    cout << "fork failed" << endl;
-    return;
-  }
-  if (pid > 0) {
-    wait(NULL);
-    return;
-  }
-  return;
 }
 
 void cd(string ch){
@@ -65,14 +53,14 @@ void cd(string ch){
 void external_programs(vector <string> full_command, vector<string> path_vect,string first_arg) {
   pid_t pid = fork();
   char *arr1[full_command.size()+1];
-  for(int i = 0; i < full_command.size() ; i++ ){
+  for(unsigned int i = 0; i < full_command.size() ; i++ ){
     arr1 [i] = (char*)full_command[i].c_str();
   }
   arr1[full_command.size()] = NULL;
 
   if (pid == 0){
     char *envp[] = {NULL};
-    int i = 0;
+    unsigned int i = 0;
     string new_path;
     while (i < path_vect.size()){
       
@@ -101,6 +89,11 @@ void external_programs(vector <string> full_command, vector<string> path_vect,st
     cout << "fork failed" << endl;
     return;
   }
+  else if (pid > 0) {
+    wait(NULL);
+    childPid = pid;
+    return;
+  }
   else{
     wait(NULL);
     return;
@@ -115,28 +108,74 @@ void background_process(vector <string> full_command, vector<string> path_vect,s
 void do_dup(string fileName){
   int file_desc = open(fileName.c_str(), O_CREAT | O_WRONLY, 0666);
   if(file_desc < 0){
-    printf("Error opening the file\n");
+    cout << "Error opening the file "<< endl;
   }
-  cout << "file open successfull"<< endl;
   dup2(file_desc, 1);
-  //printf("Tester\n");
   close(file_desc);
   return;
 }
-  
 
-
-int main(int argc, char **argv) {
-  // print the string prompt without a newline, before beginning to read
-  // tokenize the input, run the command(s), and print the result
-  // do this in a loop
-  //char input[1024];
+void commandHandler(string commandstr){
   string path_string = "/bin/:/usr/bin/";
 
   vector <string> path_vector;
   path_vector.push_back("");
   path_vector.push_back("/bin/");
   path_vector.push_back("/usr/bin/");
+
+
+  vector <string> command;
+  command = tokenize(commandstr, " ");
+  if (strcmp(command[0].c_str(), "cd")==0){
+    if (command.size() > 1) {
+      cd(command[1]);
+    }
+    else {
+      cout << "expected argument to \"cd\"" << endl;
+    }
+        
+  }
+  else if (strcmp(command[0].c_str(), "pwd")==0) {
+    pwd();
+  }
+
+  else if(strcmp(command[0].c_str(), "$PATH")==0) {
+    cout << "Current PATH: " << path_string << endl;
+  }
+  else if(strcmp(command[0].c_str(), "a2path")==0) {
+    if (command.size() > 1) {
+      if(command[1].find("$PATH") == 0) {
+        int pos = command[1].find(":");
+        string path_substr = command[1].substr(pos);
+        path_string.append(path_substr);
+        path_substr = command[1].substr(pos+1);
+        path_vector.push_back(path_substr);
+      }
+      else{
+        path_string = command[1];
+      }
+    }
+  }
+  else if(strcmp(command[0].c_str(), "exit")== 0){
+    cout << "Exiting" << endl;
+    if (childPid > 0){
+      kill(childPid, SIGTERM);
+    }
+    _exit(0);
+  }
+  
+  else {
+    external_programs(command,path_vector, command[0]);
+  }
+  return;
+}
+  
+int main(int argc, char **argv) {
+  // print the string prompt without a newline, before beginning to read
+  // tokenize the input, run the command(s), and print the result
+  // do this in a loop
+  //char input[1024];
+  
 
 
   while(1){
@@ -146,55 +185,51 @@ int main(int argc, char **argv) {
     getline(cin, str);
 
     commands_str = tokenize(str, ";");
-    for (int i =0; i< commands_str.size(); ++i) {
-      vector <string> command;
-      if(commands_str[i].find(">") != -1){
+    for (unsigned int i =0; i< commands_str.size(); ++i) {
+      //vector <string> command;
+
+      // for redirecting output
+      size_t find_redirecting = commands_str[i].find(">");
+      size_t find_background = commands_str[i].find("&");
+      if(find_redirecting > 2 && find_redirecting != string::npos){
+        int saved_stdout;
+        saved_stdout = dup(1);
         int pos = commands_str[i].find(">");
         string file_name = commands_str[i].substr(pos+2);
-        cout << "position of > charecter "<< pos << " filename: "<< file_name << endl;
         do_dup(file_name);
         string redirected_command = commands_str[i].erase(pos-1, file_name.size()+3);
-        cout << "new command: " << redirected_command << endl;
+        commandHandler(redirected_command);
+        dup2(saved_stdout, 1);
+        close(saved_stdout);
       }
-      command = tokenize(commands_str[i], " ");
-      if (strcmp(command[0].c_str(), "cd")==0){
-        if (command.size() > 1) {
-          cd(command[1]);
+
+      // for background process
+      else if(find_background > 2 && find_background != string::npos){
+        int saved_stdout;
+        saved_stdout = dup(1);
+        int pos = commands_str[i].find("&");
+        string file_name = "/dev/null";
+        int file_desc = open(file_name.c_str(), O_CREAT | O_WRONLY, 0666);
+        if(file_desc < 0){
+          cout << "Error opening the file "<< endl;
         }
-        else {
-          cout << "expected argument to \"cd\"" << endl;
-        }
+        dup2(file_desc, 1);
+        dup2(file_desc, 2);
+        close(file_desc);
+
+        string redirected_command = commands_str[i].erase(pos-1);
         
-      }
-      else if (strcmp(command[0].c_str(), "pwd")==0) {
-        pwd();
+        commandHandler(redirected_command);
+        dup2(saved_stdout, 1);
+        dup2(saved_stdout, 2);
+        //cout << redirected_command << endl;
+        close(saved_stdout);
+
       }
 
-      else if(strcmp(command[0].c_str(), "$PATH")==0) {
-        cout << "Current PATH: " << path_string << endl;
-      }
-      else if(strcmp(command[0].c_str(), "a2path")==0) {
-        if (command.size() > 1) {
-          if(command[1].find("$PATH") == 0) {
-            int pos = command[1].find(":");
-            string path_substr = command[1].substr(pos);
-            path_string.append(path_substr);
-            path_substr = command[1].substr(pos+1);
-            path_vector.push_back(path_substr);
-          }
-          else{
-            path_string = command[1];
-          }
-        }
-      }
-      else if(strcmp(command[0].c_str(), "exit")== 0){
-        cout << "Exiting" << endl;
-        _exit(3);
-      }
-      
-
-      else {
-        external_programs(command,path_vector, command[0]);
+      else{
+        commandHandler(commands_str[i]);
+        
       }
     }
   }
